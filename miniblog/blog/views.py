ed735311a1a3951cd922a3d7335d939c1477037d
ayan <ayanrajput1234@gmail.com>
2021-07-08 21:08:41 +0530

    initialized home.html,about.html,contact.html

--- miniblog/blog/views.py
+++ miniblog/blog/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def home(request):
+    return render(request, "blog/home.html")
+
+
+def about(request):
+    return render(request, "blog/about.html")
+
+
+def contact(request):
+    return render(request, "blog/contact.html")
+
+
+def dashboard(request):
+    return render(request, 'blog/dashboard.html')
